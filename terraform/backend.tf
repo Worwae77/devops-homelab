dac9d88aba5d7bb516be54f7881e6dd117dd519c
J.WOR <worawut.ju@kbtg.tech>
2025-09-29 23:13:37 +0700

    Remove Terraform backend configuration files

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-homelab-tfstate"
-    key            = "terraform.tfstate"
-    region         = "ap-southeast-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
